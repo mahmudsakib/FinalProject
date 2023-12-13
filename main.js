82732009596bd50e39507a66468477a1c0898cb0
Sakib Mahmud <46654533+mahmudsakib@users.noreply.github.com>
2023-12-13 11:30:17 +0600

    city

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appi
 .then(response => response.json())
 .then(data => {
 
-    //Getting the min and max values for each day
+   
     for(i = 0; i<5; i++){
         document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
-        //Number(1.3450001).toFixed(2); // 1.35
+       
     }
 
     for(i = 0; i<5; i++){
@@ -19,7 +19,7 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appi
     }
     //------------------------------------------------------------
 
-    //Getting Weather Icons
+    
      for(i = 0; i<5; i++){
         document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
         data.list[i].weather[0].icon
@@ -35,16 +35,16 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appi
 }
 
 function DefaultScreen(){
-    document.getElementById("cityInput").defaultValue = "London";
+    document.getElementById("cityInput").defaultValue = "Dhaka";
     GetInfo();
 }
 
 
-//Getting and displaying the text for the upcoming five days of the week
+
 var d = new Date();
 var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
 
-//Function to get the correct integer for the index of the days array
+
 function CheckDay(day){
     if(day + d.getDay() > 6){
         return day + d.getDay() - 7;
